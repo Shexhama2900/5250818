c785f4eb6c4f1c09c6b834761ee71fd62f06137f
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-01-24 00:10:42 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "",  // your bot token
-    prefix: process.env.PREFIX || "!", // bot prefix
-    ownerID: process.env.OWNERID || "491577179495333903", //your discord id
-    mongourl: process.env.MONGO_URI || "", // MongoDb URL
+    token: process.env.TOKEN || "OTM0ODkwMzE4NDY4MzYyMzEw.Ye2qdQ.ks9Tz7D2zgla5JLlUDXilZD5TDM",  // your bot token
+    prefix: process.env.PREFIX || "s!", // bot prefix
+    ownerID: process.env.OWNERID || "730549617044750346", //your discord id
+    mongourl: process.env.MONGO_URI || "mongodb+srv://jubadevlopment:<g112233s>@cluster0.p6dkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDb URL
     embedColor: process.env.COlOR || "#303236", // embed colour
-    logs: process.env.LOGS || "875254787191504947", // channel id for guild create and delete logs
+    logs: process.env.LOGS || "934907495359586365", // channel id for guild create and delete logs
 
     nodes: [
     {
